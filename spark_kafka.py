0406851b704a9f9555d6d7153120ba0b6a73e289
Awantik Das <awantikdas@gmail.com>
2019-02-15 14:14:39 +0800

    Add files via upload

---
+++ spark_kafka.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+import time
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.appName('PySpark-App').getOrCreate()
+    df_kafka = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "spark").load()
+
+    res = df_kafka.selectExpr("CAST(value AS STRING)")
+    res.writeStream.outputMode("append").format("memory").queryName("table").start()
+
+    time.sleep(10)
+    spark.sql("select * from table").show()
+    time.sleep(10)
+    spark.sql("select * from table").show()
+    time.sleep(10)
+    d = spark.sql("select * from table")
+    d.show()
